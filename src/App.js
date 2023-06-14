c454fe5380e05feb34d7d78476e3ac8233b4f8d0
gsjang94@gamil.com <gsjang94@gmail.com>
2023-06-14 16:05:17 +0900

    feat : 라우터 생성 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import Router from './components/Router';
+import Header from './components/global/Header';
+import Navbar from './components/global/Navbar';
 import './index.css';
+import './style/global.scss';
+import './style/reset.scss';
 
 function App() {
-  return <div className="App">dd</div>;
+  return (
+    <div className="App">
+      <Header />
+      <section className="flex">
+        <Navbar />
+        <div className="mainWrap">
+          <Router />
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default App;
