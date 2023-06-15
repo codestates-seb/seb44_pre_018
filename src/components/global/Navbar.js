62752be70ebf002132ee2f0cf10dccc87fa558ea
hyuniiya <wngus7988.10@gmail.com>
2023-06-15 14:56:33 +0900

    data.json 검토 필요

--- src/components/global/Navbar.js
+++ src/components/global/Navbar.js
@@ -1,5 +1,28 @@
+import { BrowserRouter, Link } from 'react-router-dom';
+
 const Navbar = () => {
-  return <div className="w-52 h-screen bg-black "></div>;
+  return (
+    <BrowserRouter>
+      <div className="w-52 h-screen bg-pointCol03">
+        <div className="fixed top-24 left-0 w-52 h-screen bg-pointCol03">
+          <div className="flex flex-col justify-center items-center">
+            <Link to="/questions" className="navButton mt-5 mb-5">
+              Questions
+            </Link>
+            <Link to="/user" className="navButton mt-5 mb-5">
+              User
+            </Link>
+            <Link to="/tag" className="navButton mt-5 mb-5">
+              Tag
+            </Link>
+            <Link to="/mypage" className="navButton mt-5 mb-5">
+              MyPage
+            </Link>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default Navbar;
