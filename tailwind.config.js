16177267f2dcc8d66ecf8c4f07cceb04a32dcd28
gsjang94@gamil.com <gsjang94@gmail.com>
2023-06-15 10:11:30 +0900

    ✨ feat : 포인트컬러 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pointCol01: '#31A8FF',
+        pointCol02: '#002075',
+        pointCol03: '#C2D3FF',
+        pointCol04: '#E9EFFF',
+        pointCol05: '#E9EFFF',
+      },
+    },
   },
   plugins: [],
 };
