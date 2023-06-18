e8a1bdc7096d8514f90caeeacd645c6ff70e1154
gsjang94@gamil.com <gsjang94@gmail.com>
2023-06-18 21:33:02 +0900

    test : axios 테스트

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,13 +1,28 @@
+import { useEffect, useState } from 'react';
 import SearchBar from '../components/SearchBar';
 import UserItem from '../components/UserItem';
 import Pagination from '../components/global/Pagination';
+import axios from 'axios';
 
 const User = () => {
+  const [users, setUsers] = useState([]);
+  const getUserData = async () => {
+    try {
+      const result = await axios.get('/data/member.json');
+      console.log(result.data);
+      setUsers(result.data.member);
+    } catch (err) {
+      console.log('err', err);
+    }
+  };
+  useEffect(() => {
+    getUserData();
+  }, []);
   return (
     <div className="inner">
       <h3 className="maintitle mb-4">User</h3>
       <SearchBar />
-      <UserItem />
+      <UserItem users={users} />
       <Pagination />
     </div>
   );
