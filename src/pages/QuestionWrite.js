9860809f79252d81ee2036f343f56f78fb2fd1ae
gsjang94@gamil.com <gsjang94@gmail.com>
2023-06-21 14:46:20 +0900

    style : 수정

--- src/pages/QuestionWrite.js
+++ src/pages/QuestionWrite.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import axios from 'axios';
 import Editor from 'components/global/questionItem/Editor';
 import AddTag from 'components/global/tag/AddTag';
 
@@ -7,6 +8,21 @@ const QuestionWrite = () => {
   const [body, setBody] = useState('');
   const [tags, setTags] = useState('');
 
+  const changeTitle = (e) => {
+    setTitle(e.currentTarget.value);
+  };
+  const submitForm = async () => {
+    if (title === '') {
+    }
+    const {
+      data: { questionId },
+    } = await axios.post('/api/question/write', {
+      title: title,
+      body: body,
+      tags: tags.join(' '),
+    });
+    navigate(`/detail/${questionId}`);
+  };
   return (
     <div className="inner">
       <h3 className="maintitle">Ask a public question</h3>
@@ -15,7 +31,13 @@ const QuestionWrite = () => {
           <label className="labelText" htmlFor="title">
             Title
           </label>
-          <input type="text" id="title" className="inputBox" />
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={changeTitle}
+            className="inputBox"
+          />
         </div>
         <div>
           <label className="labelText" htmlFor="body">
@@ -29,7 +51,7 @@ const QuestionWrite = () => {
           </label>
           <AddTag tags={tags} setTags={setTags} />
         </div>
-        <button type="submit" className="pointBu03 my-12">
+        <button type="submit" className="pointBu03 my-12" onClick={submitForm}>
           Post your question
         </button>
       </div>
