4f89ee134a6610fd6b4d5840e33ca36eea27c4b4
gsjang94@gamil.com <gsjang94@gmail.com>
2023-06-23 20:22:32 +0900

    feat : 질문 수정폼 추가

---
+++ src/pages/QuestionUpdate.js
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Editor from 'components/global/questionItem/Editor';
+import AddTag from 'components/global/tag/AddTag';
+
+const QuestionUpdate = () => {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [tags, setTags] = useState('');
+  const [checkTitle, setCheckTitle] = useState(false);
+  const [checkBody, setCheckBody] = useState(false);
+  const [checkTag, setCheckTag] = useState(false);
+
+  const titleChange = (e) => {
+    setCheckTitle(false);
+    setTitle(e.currentTarget.value);
+  };
+  const tagChange = (e) => {
+    setCheckTag(false);
+  };
+  const submitForm = async () => {
+    setCheckTitle(title === '');
+    setCheckBody(body === '');
+    try {
+      const result = await axios.get('question/update/2/1', {
+        title: title,
+        content: body,
+      });
+      navigate(`/detail/1`);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div className="inner">
+      <h3 className="maintitle">Ask a public question</h3>
+      <div>
+        <div>
+          <label className="labelText" htmlFor="title">
+            Title
+          </label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={titleChange}
+            className={`inputBox ${checkTitle && 'disabledInput'}`}
+          />
+          {checkTitle && <p className="notice">제목은 필수 입력해야 합니다.</p>}
+        </div>
+
+        <div>
+          <label className="labelText" htmlFor="body">
+            Body
+          </label>
+          <Editor
+            height="400"
+            value={body}
+            setValue={setBody}
+            checkBody={setCheckBody}
+          />
+          {checkBody && <p className="notice">본문은 필수 입력해야 합니다.</p>}
+        </div>
+
+        <div>
+          <label className="labelText" htmlFor="tag">
+            Tag
+          </label>
+          <AddTag tags={tags} setTags={setTags} checkTag={setCheckTag} />
+          {checkTag && <p className="notice">태그는 필수 입력해야 합니다.</p>}
+        </div>
+        <button type="submit" className="pointBu03 my-12" onClick={submitForm}>
+          Post your question
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default QuestionUpdate;
