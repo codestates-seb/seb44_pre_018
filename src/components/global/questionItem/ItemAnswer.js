fed3787d9de6aa6542f9d35c1b1dae9c706aeb7f
gsjang94@gamil.com <gsjang94@gmail.com>
2023-06-26 17:18:55 +0900

    feat : 리스트 아이템 댓글 개수 수정

--- src/components/global/questionItem/ItemAnswer.js
+++ src/components/global/questionItem/ItemAnswer.js
@@ -1,10 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMessage } from '@fortawesome/free-solid-svg-icons';
 
-const ItemAnswer = ({ commentCount }) => {
+const ItemAnswer = ({ viewAnswer }) => {
   return (
     <div>
-      <span className="font-light mr-1"> {commentCount} </span>
+      <span className="font-light mr-1"> {viewAnswer} </span>
       <FontAwesomeIcon icon={faMessage} />
     </div>
   );
